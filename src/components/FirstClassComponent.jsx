dff9c465283d0961a8370137720f89e3caa10f81
Eugene Polyakov <jenick12345@yandex.ru>
2023-06-22 16:53:41 +0300

    useMemo implemented

--- src/components/FirstClassComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class FirstClassComponent extends Component {
-    render() {
-        return <div></div>;
-    }
-}
-
-export default FirstClassComponent;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx';
 import './index.css';
 import FirstComponent from './components/FirstFuncComponent.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SecondFuncComponents from './components/SecondFuncComponents.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App />}></Route>
+            <Route path="/" element={<SecondFuncComponents />}></Route>
             <Route path="firstComponent" element={<FirstComponent />}></Route>
         </Routes>
     </BrowserRouter>
